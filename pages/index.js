730bddc184f7096a574da4c302a2c8d8ca264ef6
Leon Graff <442graff@gmail.com>
2023-08-13 06:57:13 -0300

    author

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+//next image
+import Image from "next/image";
+
+//components
+import ParticlesContainer from '../components/ParticlesContainer'
+import ProjectsBtn from '../components/ProjectsBtn'
+import Avatar from '../components/Avatar'
+
+//framer motion
+import { motion } from 'framer-motion'
+
+//variants
+import { } from '../variants'
+
 const Home = () => {
   return <div>Home</div>;
 };
